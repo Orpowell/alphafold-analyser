2849dc4ed001a3b71eef11cb80b736a14c696413
Oliver Powell <mail@oliverpowell.com>
2025-05-18 22:25:45 +0100

    Split commands
    Split the three core functionalities into 3 different commands:
    - pae to plot the PAE heatmap
    - plddt to plot the PLDDT graph
    - structure to visualise the plddt on a pdb

--- src/alphafold_analyser/__main__.py
+++ src/alphafold_analyser/__main__.py
@@ -9,16 +9,50 @@ from .utils import *
 from .__init__ import __version__
 
 def main():
+    
     parser = argparse.ArgumentParser(
         prog="AlphaFold Analyser",
         add_help=False,
         formatter_class=argparse.RawTextHelpFormatter,
     )
-
-    group_inputs = parser.add_argument_group("Inputs")
     
-    # Get pdb structure path
-    group_inputs.add_argument(
+    sub_parsers = parser.add_subparsers(dest="command")
+
+    # Versioning
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"{parser.prog} v{__version__}"
+    )
+    
+    # Help
+    parser.add_argument(
+        "-h", "--help", action="help", help="show this help message and exit\n "
+    )
+    
+    common_inputs_parser = argparse.ArgumentParser(add_help=False)
+    
+    common_inputs_parser.add_argument(
+        "-l",
+        "--pkl",
+        metavar="\b",
+        type=lambda x: is_valid_file(parser, x),
+        action="store",
+        help="path to pkl file - can generate both PAE and PLDDT plots",
+        required=True
+    )
+    
+    common_inputs_parser.add_argument(
+        "-o",
+        "--output",
+        metavar="\b",
+        type=str,
+        action="store",
+        help="directory to store all generated outputs",
+        default="analyser_output",
+    )
+    
+    protein_input_parser = argparse.ArgumentParser(add_help=False)
+    
+    protein_input_parser.add_argument(
         "-p",
         "--pdb",
         metavar="\b",
@@ -28,19 +62,7 @@ def main():
         default=None
     )
     
-    # Get pkl file path
-    group_inputs.add_argument(
-        "-l",
-        "--pkl",
-        metavar="\b",
-        type=lambda x: is_valid_file(parser, x),
-        action="store",
-        help="path to pkl file - generates predicted aligned error plot",
-        default=None,
-    )
-
-    # Get pkl file path
-    group_inputs.add_argument(
+    protein_input_parser.add_argument(
         "-b",
         "--binary",
         metavar="\b",
@@ -49,75 +71,45 @@ def main():
         help="path to pymol binary - Only required for analysing pdb (-p)",
         default=None,
     )
-
-    group_output = parser.add_argument_group("Outputs")
     
-    # Get output directory
-    group_output.add_argument(
-        "-o",
-        "--output",
-        metavar="\b",
-        type=str,
-        action="store",
-        help="directory to store all generated outputs",
-        default="analyser_output",
-    )
-
-    group_options = parser.add_argument_group("Options")
+    plddt_parser = sub_parsers.add_parser("plddt", parents=[common_inputs_parser])
+    pae_parser = sub_parsers.add_parser("pae", parents=[common_inputs_parser])
+    structure_parser = sub_parsers.add_parser("structure", parents=[common_inputs_parser, protein_input_parser])
     
-    # Get Version
-    group_options.add_argument(
-        "-v", "--version", action="version", version=f"{parser.prog} v{__version__}"
-    )
-    
-    # Get help
-    group_options.add_argument(
-        "-h", "--help", action="help", help="show this help message and exit\n "
-    )
-
     # Parse arguments
     args = parser.parse_args()
-
+    
     # If all arguments are None display help text by parsing help
     
-    if (args.pkl is None) and (args.pdb is None):
+    if (args.pkl is None):
         parser.parse_args(["-h"])
-
+    
     # Check output directory exists
     if not os.path.exists(args.output):
         try:
             os.makedirs(args.output)
-            print(f"\nCreated output directory: {args.output}")
+            
         except Exception as e:
             print(f"\nERROR: failed to create output directory {args.output}: {str(e)}")
             sys.exit(1)
-    else:
-        print(f"\nOutput directory: {args.output}")
     
-    #Check binary provided if pymol file is provided
-    if args.pdb is not None and args.binary is None:
-        parser.error("\nERROR: pymol binary required to analyse protein structure.")
-
-    if args.pdb is not None:
+    print(f"\nOutput directory: {args.output}")
+    
+    if args.command == "plddt":
+        print("\nPlotting plddt...")
+        plot_pLDDT(args.pkl, args.output)
+    
+    if args.command == 'pae':
+        print("\nPlotting predicted aligned error...")
+        plot_PAE(args.pkl, args.output) 
+    
+    if args.command == 'structure':
         print("\nVisualising pLDDT data...")
         protein_painter(args.pdb, args.output, args.binary)
 
-    elif args.pdb is None:
-        print("\nNo pdb file provided, skipping pLDDT data visualisation...")
-
-    if args.pkl is not None:
-        print("\nPlotting predicted aligned error and plddt...")
-        plot_PAE(args.pkl, args.output)
-        plot_pLDDT(args.pkl, args.output)
-        
-    elif args.pkl is None:
-        print(
-            "\nNo pickle file provided, skipping predicted aligned error visualisation..."
-        )
-        
-    print("\nAll processes finished, shutting down.")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
     main()
-    sys.exit(0)
+   
